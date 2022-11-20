f7b26b39517713898a1a800bc0aff471f06d3bea
bestcondition <admin@bestcondition.cn>
2022-11-21 00:44:37 +0800

    提取默认参数

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
 
+from config import config
+
 app = Flask(__name__, template_folder='template')
 
 
 @app.route('/')
 def index():
     kwargs = dict(
-        first='AA',
-        second='BB',
-        title='heart',
-        alert=True,
+        first=config.FIRST,
+        second=config.SECOND,
+        title=config.TITLE,
+        alert=config.ALERT,
     )
     kwargs.update(request.args)
-    kwargs.update(alert=kwargs['alert'] != '0')
+    kwargs.update(alert=kwargs['alert'] != config.NOT_ALERT_VALUE)
     return render_template(
         template_name_or_list='index.html',
         **kwargs,
--- config.py
+++ config.py
@@ -5,6 +5,11 @@ from dataclasses import dataclass, asdict
 class MyConfig:
     PORT: int = 5000
     HOST: str = '0.0.0.0'
+    FIRST: str = 'first'
+    SECOND: str = 'second'
+    ALERT: bool = True
+    TITLE: str = 'title'
+    NOT_ALERT_VALUE: str = '0'
 
     def to_dict(self) -> dict:
         return asdict(self)
